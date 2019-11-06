7e22973eb080464ea04ba883d2b0dce9397dc4d3
sigits12 <supratmanto12@gmail.com>
2019-11-06 22:53:59 +0700

    each episode

---
+++ vue-beginner/app.js
@@ -0,0 +1,23 @@
+new Vue ({
+    el:'#app',
+    data: {
+        age: 24,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        add: function (inc) {
+            this.age += inc;
+        },
+        subtract: function (dec) {
+            this.age -= dec;
+        },
+        updateXY: function (event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        click: function () {
+            alert("You clicked me!")
+        }
+    }
+});
---
+++ vue-loop/app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data: {
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Browser'],
+        ninjas: [
+            { name: 'Ryu', age: 25},
+            { name: 'Yoshi', age: 35},
+            { name: 'Ken', age: 55},
+        ]
+    },
+    methods: {
+        
+    },
+    computed: {
+        
+    }
+});
