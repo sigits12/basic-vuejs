3bb1ca28d5156d25be322a225f6726e62b2fc2ec
sigits12 <supratmanto12@gmail.com>
2019-11-22 07:08:14 +0700

    vue cli

---
+++ vue-playlist/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      title: 'Your first Vue file, wooo!',
+    }
+  },
+  methods: {
+    greeting() {
+      return 'hey cowboy!';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
